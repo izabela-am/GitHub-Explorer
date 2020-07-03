d04e066e788975a6be6710c7455c10dcf6ebfa75
Izabela <izabela.m.alfenas@gmail.com>
2020-07-03 20:58:11 -0300

    create and style first page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => (
-    <h1>Dashboard</h1>
+    <>
+        <img src={logoImg} alt="Logo"/>
+        <Title>Explore repositórios no GitHub</Title>
+
+        <Form>
+            <input placeholder='Nome do repositório' type="text"/>
+            <button type="submit">Pesquisar</button>
+        </Form>
+
+        <Repositories>
+            <a href="teste">
+                <img
+                    src="https://avatars2.githubusercontent.com/u/40214340?s=460&u=fc8c728e6b25e56531f721cb599b82aeb9c20524&v=4"
+                    alt="Izabela"
+                />
+                <div>
+                    <strong>Repositorio</strong>
+                    <p>Descrição do meu repositório vem aqui</p>
+                </div>
+                <FiChevronRight size={20}/>
+            </a>
+        </Repositories>
+    </>
 );
 
 export default Dashboard;
